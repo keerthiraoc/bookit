6428f69c87c12527936e28009e6758d5eb4639ae
Keerthi Rao <keerthirao27@gmail.com>
2025-01-31 12:25:28 +0530

    Room Details Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Available Rooms" />
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3>{room.name}</h3>)
+        rooms.map((room) => <RoomCard room={room} />)
       ) : (
         <p>No rooms available at the moment</p>
       )}
