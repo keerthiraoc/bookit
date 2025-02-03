f48992f4da1804238a77c6ba07a06084b771b73c
Keerthi Rao <keerthirao27@gmail.com>
2025-02-03 14:43:14 +0530

    feature: cancel Booking

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -7,7 +7,7 @@ const BookingsPage = async () => {
 
   return (
     <>
-      <Heading title="Bookings" />
+      <Heading title="My Bookings" />
       {bookings.length === 0 ? (
         <p className="text-gray-600 mt-4">You have no bookings</p>
       ) : (
