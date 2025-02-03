d7a3158f5b8e965f353d4c1355eaa6e0f69be61f
Keerthi Rao <keerthirao27@gmail.com>
2025-02-03 13:58:26 +0530

    feature: show Bookings

---
+++ app/actions/getMyBookings.js
@@ -0,0 +1,40 @@
+"use server";
+
+import { createSessionClient } from "@/config/appwrite";
+import { cookies } from "next/headers";
+import { Query } from "node-appwrite";
+import { redirect } from "next/navigation";
+import checkAuth from "./checkAuth";
+
+async function getMybookings() {
+  const sessionCookie = (await cookies()).get("appwrite-session");
+  if (!sessionCookie) {
+    redirect("/login");
+  }
+
+  try {
+    const { databases } = await createSessionClient(sessionCookie.value);
+
+    const { user } = await checkAuth();
+    if (!user) {
+      return {
+        error: "You must be logged in to view bookings"
+      };
+    }
+
+    const { documents: bookings } = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATEBASE,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_BOOOKINGS,
+      [Query.equal("user_id", user.id)]
+    );
+
+    return bookings;
+  } catch (error) {
+    console.log("Failed to get user bookings", error);
+    return {
+      error: "Failed to get bookings"
+    };
+  }
+}
+
+export default getMybookings;
--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,5 +1,22 @@
-const BookingsPage = () => {
-  return <>Bookings</>;
+import Heading from "@/components/Heading";
+import getMybookings from "../actions/getMyBookings";
+import BookedRoomCard from "@/components/BookedRoomCard";
+
+const BookingsPage = async () => {
+  const bookings = await getMybookings();
+
+  return (
+    <>
+      <Heading title="Bookings" />
+      {bookings.length === 0 ? (
+        <p className="text-gray-600 mt-4">You have no bookings</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+      )}
+    </>
+  );
 };
 
 export default BookingsPage;
